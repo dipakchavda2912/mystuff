9c9ecf28e0ba2d57b538ce80bd1fa84271cc741a
DipakChavda <dipak.chavda2912@gmail.com>
2015-02-12 15:44:31 +0530

    Google fonts integration

---
+++ googlefonts/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="js/modernizr.js"></script>
+        <link type="text/css" rel="stylesheet" href="css/normalize.css"/>
+        <link type="text/css" rel="stylesheet" href="css/fonts.css"/>
+        <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    </head>
+    <body>
+        <div>
+            <section>
+                <article>
+                    <p>
+                        Firefox likes to assume when you click on a link to an RSS feed that 
+                        you want to subscribe to it in some fashion. That might be true most 
+                        of the time, but sometimes you just want to see the source of the RSS.
+                        To view the source of an RSS feed, just append "view-source:" in the 
+                        address bar before the feed address
+                    </p>
+                </article>
+            </section>
+        </div>
+    </body>
+</html>
